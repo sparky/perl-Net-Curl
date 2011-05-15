509ad35bbba4bbb585fc14da6481f629b0d6555f
sparky <sparky@pld-linux.org>
2011-05-15 22:10:56 +0200

    - ver 0.24

--- lib/Net/Curl.pm
+++ lib/Net/Curl.pm
@@ -6,7 +6,7 @@ use Exporter 'import';
 our @ISA;
 our $VERSION;
 BEGIN {
-	$VERSION = '0.23';
+	$VERSION = '0.24';
 
 	my $loaded = 0;
 
--- lib/Net/Curl/Share.pm
+++ lib/Net/Curl/Share.pm
@@ -5,7 +5,7 @@ use warnings;
 use Net::Curl ();
 use Exporter 'import';
 
-our $VERSION = '0.23';
+our $VERSION = '0.24';
 
 our @EXPORT_OK = grep /^CURL/, keys %{Net::Curl::Share::};
 our %EXPORT_TAGS = ( constants => \@EXPORT_OK );
