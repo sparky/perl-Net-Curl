5854c1d92af4218f81c00acad61aa3deaa710764
Stanislaw Pusep <creaktive@gmail.com>
2014-05-30 17:42:02 +0200

    Bumped version

--- lib/Net/Curl.pm
+++ lib/Net/Curl.pm
@@ -8,7 +8,7 @@ use Exporter 'import';
 our @ISA;
 our $VERSION;
 BEGIN {
-	$VERSION = 0.33;
+	$VERSION = 0.34;
 
 	my $loaded = 0;
 
--- lib/Net/Curl/Share.pm
+++ lib/Net/Curl/Share.pm
@@ -5,7 +5,7 @@ use warnings;
 use Net::Curl ();
 use Exporter 'import';
 
-our $VERSION = 0.33;
+our $VERSION = 0.34;
 
 our @EXPORT_OK = grep { /^CURL/x } keys %{Net::Curl::Share::};
 our %EXPORT_TAGS = ( constants => \@EXPORT_OK );
