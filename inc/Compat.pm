56795d2b2ee04ee105dc47c645367f0356379f22
Stanislaw Pusep <creaktive@gmail.com>
2022-07-08 11:02:02 +0200

    Bumped version

--- inc/Compat.pm
+++ inc/Compat.pm
@@ -69,7 +69,7 @@ VERSION
 # Dirty hack so Test::ConsistentVersion passes
 sub VERSION {
 	return (caller)[0] eq 'Test::ConsistentVersion'
-		? '0.50'
+		? '0.51'
 		: $VERSION;
 }
 
--- lib/Net/Curl.pm
+++ lib/Net/Curl.pm
@@ -8,7 +8,7 @@ use Exporter 'import';
 our @ISA;
 our $VERSION;
 BEGIN {
-	$VERSION = '0.50';
+	$VERSION = '0.51';
 
 	my $loaded = 0;
 
